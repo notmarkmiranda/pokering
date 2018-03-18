5b49f5af6eddc4bee80efd05cb615c80e7dacee7
mark miranda <notmarkmiranda@gmail.com>
2018-03-18 07:39:40 -0600

    Add foundation & style devise views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+//= require foundation
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
