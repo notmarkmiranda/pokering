8955060b9ecf324a339d37d1dae2930542aac223
mark miranda <notmarkmiranda@gmail.com>
2018-03-26 07:55:15 -0600

    Add testing for season resource

--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -9,5 +9,31 @@ RSpec.describe Season, type: :model do
     it { should validate_presence_of :league_id }
   end
 
-  context 'methods'
+  context 'methods' do
+    let(:season) { create(:season) }
+
+    context 'active / complete methods' do
+      it 'active & complete = true' do
+        season.update(active: true, completed: true)
+
+        expect(season.activate_button('active')).to eq('times-circle')
+        expect(season.activate_button('completed')).to eq('times-circle')
+        expect(season.active_text).to eq('Active')
+        expect(season.active_title).to eq('Mark as Inactive')
+        expect(season.completed_text).to eq('Completed')
+        expect(season.completed_title).to eq('Mark as Incomplete')
+      end
+
+      it 'active & complete = false' do
+        season.update(active: false, completed: false)
+
+        expect(season.activate_button('active')).to eq('check-circle')
+        expect(season.activate_button('completed')).to eq('check-circle')
+        expect(season.active_text).to eq('Inactive')
+        expect(season.active_title).to eq('Mark as Active')
+        expect(season.completed_text).to eq('Incomplete')
+        expect(season.completed_title).to eq('Mark as Complete')
+      end
+    end
+  end
 end
