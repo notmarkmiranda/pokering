61f81b9e89067f6e13f6762a9e7b73a379e43397
mark miranda <notmarkmiranda@gmail.com>
2018-03-27 19:36:21 -0600

    Game Resource

---
+++ spec/models/player_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Player, type: :model do
+  context 'relationships' do
+    it { should belong_to :game }
+    it { should belong_to :user }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of :game_id }
+    it { should validate_presence_of :user_id }
+    it do
+      user = create(:user, email: 'asdf@gmail.com')
+      create(:player, user: user)
+
+      should validate_uniqueness_of(:user_id).scoped_to(:game_id)
+    end
+  end
+  context 'methods'
+end
